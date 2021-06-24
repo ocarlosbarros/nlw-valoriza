4241dc4c3ae04e7c1cd95e354d32fdf1d4f2042d
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-24 13:01:12 -0300

    Adiciona typeorm e configura o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,27 @@
-/***
+/**
  * @author Carlos Barros
  * @version 1.0.0
  * @since 06/23/2021
  */
 
 import express from "express";
+import "reflect-metadata";
+
+//Import config database
+import "./database";
 
 //Cria uma instância do express chamada app
 const app = express();
 
-/**
- *  Métodos http:
- *  get => Buscar uma informação
- *  POST  => Inserir (criar) uma informação
- *  PUT =>  Alterar uma informação
- * DELETE =>  Remover um dado
- * PATCH  =>  Alterar uma informação específica
- */
-
 //Criando uma rota GET
+/**
+ * @param {string} - rota da aplicação
+ * @param {function} - request (entrada), response (saída)
+ * @returns {string} - uma resposta simples através do método send.
+ */
 app.get("/", (request, response) => {
-  //Resquest => entrada
-  //Response  => saída
-  //Retorna uma resposta simples
   return response.send('Olá NLW');
 });
-
-app.post("/test-post", (request, response) => {
-  return response.send('Olá NLW método POST');
-})
 
 //Inicializa o servidor passando uma porta = http://localhost:3000
 app.listen(3000, () => console.log("Server is running..."));
