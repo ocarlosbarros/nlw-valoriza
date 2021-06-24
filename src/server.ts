f9366088c1e7532f9cdacd8700062a7dd7b1722a
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-23 22:25:42 -0300

    Adiciona metodo GET para rota principal da aplicaçao

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,22 @@ import express from "express";
 //Cria uma instância do express chamada app
 const app = express();
 
+/**
+ *  Métodos http:
+ *  get => Buscar uma informação
+ *  POST  => Inserir (criar) uma informação
+ *  PUT =>  Alterar uma informação
+ * DELETE =>  Remover um dado
+ * PATCH  =>  Alterar uma informação específica
+ */
+
+//Criando uma rota GET
+app.get("/", (request, response) => {
+  //Resquest => entrada
+  //Response  => saída
+  //Retorna uma resposta simples
+  return response.send('Olá NLW');
+});
+
 //Inicializa o servidor passando uma porta = http://localhost:3000
 app.listen(3000, () => console.log("Server is running..."));
