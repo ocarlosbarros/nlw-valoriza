229f228846546ec987a57bf5aeb80aa11d9a2be4
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-26 12:04:30 -0300

    Cria a rota para criacao de tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express"
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 
 const router = Router();
 
+//Cadastro de Usuário
 const createUserController = new CreateUserController();
-
 router.post("/users", createUserController.handle);
+
+//Casdastro de Tags
+const createTagController = new CreateTagController();
+router.post("/tags", createTagController.handle);
 
 export { router }
