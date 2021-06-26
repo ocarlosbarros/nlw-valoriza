38409c5dbb123fbb51251b59eb3426c76105f9bf
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-26 19:53:42 -0300

    Criado service de listagem de compliments recebidos

---
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from "typeorm";
+import { ComplimentRepository } from "../repositories/ComplimentRepository";
+
+class ListUserReceiveComplimentsService {
+
+  async execute(user_id: string) {
+    const complimentRepository = getCustomRepository(ComplimentRepository);
+
+    const compliments = await complimentRepository.find({
+      where: {
+        user_receiver: user_id
+      }
+    })
+    return compliments;
+  }
+
+}
+
+export { ListUserReceiveComplimentsService };
