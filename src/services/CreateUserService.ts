b9fa06aa49e85a35f46cfc51ef9fbed383b202e9
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-26 09:46:46 -0300

    Adiciona CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,43 @@
+import { UserRepository } from "../repositories/UserRepository"
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+
+  async execute({ name, email, admin }: IUserRequest) {
+    const userRepository = new UserRepository();
+
+    //Verifica se o email esta vazio e lança uma exceçao
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    //Verifica se existe um usuário cadastrado;
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    });
+
+    //Lança uma exceção caso usuário já existe
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    //Cria uma instância do usuário
+    const user = userRepository.create({
+      name,
+      email,
+      admin
+    });
+
+    //Salva o usuário no banco de dados
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService }
