806786fca95e75dba18b62fa1e0e4594fabd100f
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-26 08:55:53 -0300

    Definido atributos da entidade User e importado a biblioteca uuid para gerar os ids automaticamente

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,8 +1,32 @@
-import { Entity } from "typeorm";
-
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
 @Entity("users")
 class User {
 
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    //se o usuário não existir vou criar um uuid para criaçao do usuario
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 
 }
 
