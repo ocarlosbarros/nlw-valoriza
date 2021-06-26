e9658010de2434fac5ef8278adcc1ede25618edc
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-26 17:31:38 -0300

    Criada rota para cadastro de compliments

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
@@ -18,5 +19,9 @@ router.post("/tags", ensureAdmin, createTagController.handle);
 //Login de Usuário
 const authenticateUserController = new AuthenticateUserController();
 router.post("/login", authenticateUserController.handle);
+
+//Cadastro de Compliment
+const createComplimentController = new CreateComplimentController();
+router.post("/compliments", createComplimentController.handle);
 
 export { router }
