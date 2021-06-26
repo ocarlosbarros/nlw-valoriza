f7a03be8bb68fbc2c903c5c5d71bb27860cea352
Carlos Barros <carlos_dbs@hotmail.com>
2021-06-26 11:17:28 -0300

    Tratamento de erros com middlewares

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -4,6 +4,7 @@ import { CreateUserService } from "../services/CreateUserService";
 class CreateUserController {
 
   async handle(request: Request, response: Response) {
+
     const { name, email, admin } = request.body;
 
     const createUserService = new CreateUserService();
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@
  * @since 06/23/2021
  */
 
-import express from "express";
 import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
+
 //Import config database
 import "./database";
 
@@ -15,6 +17,19 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+//Middleare para tratar erros
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    });
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 //Criando uma rota GET
 /**
